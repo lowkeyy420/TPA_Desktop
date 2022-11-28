3a70cb3ea499b3b15892dab73842cc74836f44d0
Anderson <jonainjosafat@yahoo.com>
2022-11-28 18:02:38 +0700

    init

---
+++ stuckinthemovie/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={
+            "https://c-fa.cdn.smule.com/rs-s53/arr/02/ce/ace123e2-2a2b-4b66-93c3-bdb22ccc3d60_1024.jpg"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
